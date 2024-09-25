3132b41d1619973824edb53d3dfe565c5d7c239d
murk0s <m.kamneva@gmail.com>
2024-09-25 13:48:18 +0400

    working tests

--- src/main/java/ru/skillfactorydemo/tgbot/repository/StatsRepository.java
+++ src/main/java/ru/skillfactorydemo/tgbot/repository/StatsRepository.java
@@ -1,7 +1,6 @@
 package ru.skillfactorydemo.tgbot.repository;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -23,9 +22,10 @@ public class StatsRepository {
 
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
     public int getCountIncomesThatGreaterThan(BigDecimal amount){
+
         Map<String, Object> parametrs = new HashMap<>();
         parametrs.put("amount", amount);
-        return namedParameterJdbcTemplate.queryForObject("SELECT count(*) FROM incomes WHERE income > :amount", parametrs, new StatsRowMapper());
+        return namedParameterJdbcTemplate.queryForObject("SELECT COUNT(*) AS COUNT FROM INCOMES WHERE INCOMES.income > :amount", parametrs, new StatsRowMapper());
     }
 
     private static final class StatsRowMapper implements RowMapper <Integer> {
@@ -39,7 +39,7 @@ public class StatsRepository {
     public int getCountSpendsThatGreaterThan(BigDecimal amount){
         Map<String, Object> parametrs = new HashMap<>();
         parametrs.put("amount", amount);
-        return namedParameterJdbcTemplate.queryForObject("SELECT count(*) FROM spend WHERE spend > :amount", parametrs, new StatsRowMapper());
+        return namedParameterJdbcTemplate.queryForObject("SELECT COUNT(*) AS COUNT FROM SPEND WHERE SPEND.spend > :amount", parametrs, new StatsRowMapper());
     }
 
 }
--- src/test/java/ru/skillfactorydemo/tgbot/repository/SpendRepositoryTest.java
+++ src/test/java/ru/skillfactorydemo/tgbot/repository/SpendRepositoryTest.java
@@ -5,18 +5,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import ru.skillfactorydemo.tgbot.entity.Income;
+import ru.skillfactorydemo.tgbot.entity.Spend;
 
 import java.util.List;
 
 @DataJpaTest
-public class IncomeRepositoryTest {
+public class SpendRepositoryTest {
     @Autowired
-    private IncomeRepository incomeRepository;
+    private SpendRepository spendRepository;
 
     @Test
     public void TestRepo() {
-        final List<Income> found = incomeRepository.findAll();
-        Assert.assertEquals(2, found.size());
-
+        final List<Spend> found = spendRepository.findAll();
+        Assert.assertEquals(3, found.size());
     }
 }
